4dac9a12a6252313610fe60f0dbd7f40feea3922
lorena.miralles <lorena.miralles@estudiantat.upc.edu>
2022-05-19 18:14:29 +0200

    add default addresses

--- main_server.py
+++ main_server.py
@@ -3,7 +3,11 @@ import sys
 
 
 if __name__ == "__main__":
-	server_ip = sys.argv[1]
-	server_port = int(sys.argv[2])
+	if len(sys.argv) == 1:
+		server_ip = '127.0.0.1'
+		server_port = 8080
+	else:
+		server_ip = sys.argv[1]
+		server_port = int(sys.argv[2])
 	s = Server(server_ip, server_port)
 	s.listen()
