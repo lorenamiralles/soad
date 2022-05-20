44297408ff120afef32fac4a46e015080cfdd31e
Francesc Holly <2012.1c.holfrancesc@gmail.com>
2022-05-20 15:54:46 +0200

    Create first version of client. TODO: tests

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import os
 
 
 class Client:
@@ -7,16 +8,63 @@ class Client:
 		self.server_ip = server_ip
 		self.client_port = client_port
 		self.server_port = server_port
+	
+	def print_usage(self):
+		print(
+		"""
+		___________________________________________________________________________________
+		|																				  |
+		|					   AN ERROR OCCURED WITH YOUR JOB REQUEST					  |
+		|_________________________________________________________________________________|
+		|																				  |
+		| Matrix multiplications directory must contain tow files named A.txt and B.txt   |
+		| each of them having the corresponding matrices encoded as ascii files of 		  |
+		| single-space separated values, each row having the same number of elements and  |
+		| representing a row of the matrix.												  |
+		| For obvious reasons (common knowledge on matrix multiplication) A.txt must have |
+		| the same number of columns as rows has B.txt.									  |
+		|_________________________________________________________________________________|
+		""")
+	
+	def read_matrix_file(self, filepath):
+		inp_error = False
+		matrix = []
+		with open(filepath, 'r') as file_a:
+			matrix = [row.split(' ') for row in file_a.readlines()]
+			if any(len(matrix[0])!= len(row) for row in matrix):
+				inp_error = True
+		return matrix, inp_error
 
 	def connect(self):
 		s = socket.socket()  # Create a socket object
 		s.connect((self.server_ip, self.server_port))  # Bind to the port
 		s.send(b'c')
 		while True:
-			msg = input('> ')
-			s.send(bytes(msg, 'utf-8'))
-			# id = 123
-			# a = [[1, 2], [3,4]]
-			# b = [[4, 5], [6,7]]
-			# data = json.dumps({"id": id, "a": a, "b": b})
-			# s.send(data.encode())
+			inp = input('> ')
+			if inp == 'exit':
+				s.send(bytes('exit', 'utf-8'))
+				exit()
+
+			if inp[0] == '/':
+				directory = inp
+			else:
+				directory = os.path.join(os.getcwd(), inp)
+			files = os.listdir(directory)
+
+			if not ('A.txt' in files and 'B.txt' in files):
+				self.print_usage()
+				continue
+			
+			matrix_a, inp_error = self.read_matrix_file(os.path.join(directory,'A.txt'))
+			if inp_error:
+				self.print_usage()
+				continue
+			
+			matrix_b, inp_error = self.read_matrix_file(os.path.join(directory,'B.txt'))
+			if inp_error:
+				self.print_usage()
+				continue
+			
+			job_data = json.dumps({'id': directory, 'a': matrix_a, 'b': matrix_b})
+			s.send('job')
+			s.send(job_data.encode())
